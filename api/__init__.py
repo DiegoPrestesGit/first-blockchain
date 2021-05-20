63979510e3a70192d621de290b24667989438829
Diego Prestes <diegoprestes.git@hotmail.com>
2021-05-20 16:37:00 -0300

    refactor: add a api file

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 from app import flask_app_instance
-from .blockchain import blockchain
+from .api import api
 
 rest_server = Api(flask_app_instance)
-rest_server.add_resource(blockchain, '')
+rest_server.add_resource(api, '')
