1bbd1d9fe71087b2477dc767e51584b58a8d6bb0
Diego Prestes <diegoprestes.git@hotmail.com>
2021-05-23 20:35:54 -0300

    style: removed some unnused functions

--- api/xescoin.py
+++ api/xescoin.py
@@ -1,9 +1,6 @@
 from flask import Flask, jsonify, request
 from uuid import uuid4
-from urllib.parse import urlparse
-import requests
 
 
 class Xescoin:
-    def __init__(self):
-      
+    print('todo')
