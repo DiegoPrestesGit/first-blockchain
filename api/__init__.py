08573a1696582aa238fcdc4b2850c6871c8fe2f0
Diego Prestes <diegoprestes.git@hotmail.com>
2021-05-25 20:24:14 -0300

    config: change .gitignore and clear git cache

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Api
-from app import flask_app_instance
-from .api import api
-
-rest_server = Api(flask_app_instance)
-rest_server.add_resource(api, '')
--- api/xescoin.py
+++
@@ -1,2 +0,0 @@
-class Xescoin:
-    print('todo')
