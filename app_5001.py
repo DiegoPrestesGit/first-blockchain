396d723928203f2f4ad486644e5cfb1eca0a43c1
Diego Prestes <diegoprestes.git@hotmail.com>
2021-05-30 09:46:01 -0300

    style: removing unnecessary comments

--- app_5001.py
+++ app_5001.py
@@ -10,6 +10,5 @@ flask_app_instance = Flask(__name__)
 
 if __name__ == '__main__':
     logger.debug('starting app')
-    # from api import *
     app.run(host=host, port=port,
             debug=True, use_reloader=True)
